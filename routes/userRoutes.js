53eadc45b11c2cb9f8ae2d1178af9f05b36ee6ba
lesley <lesleybanadzem2017@gmail.com>
2020-01-17 16:38:38 -0800

    wrote a register endpoint to add a new user using the addUser model and wrote a finduserbyid model to return user with a specific id

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs')
-const { findUsers } = require('./../models/userModels')
+const { findUsers, addUser } = require('./../models/userModels')
+
+router.post('/register', async (req, res, next)=> {
+    const user = req.body;
+    try {
+        if(user){
+            res.status(201).json({
+                message: 'New user created',
+                user: await addUser(user)
+            })
+        }
+    } catch (error) {
+        res.status(401).json({
+            message:'error',
+            error: error 
+        })
+    }
+   
+})
 
 router.get('/users', async (req, res, next)=>{
     res.status(200).json({
