e8430cf564f2f912d84d9c15569217e43cd7e86c
lesley <lesleybanadzem2017@gmail.com>
2020-01-17 16:07:47 -0800

    wrote cleanup function for the db and a file to seed the db with a single user

--- database/migrations/20200117155411_user-db-00.js.js
+++ database/migrations/20200117155411_user-db-00.js.js
@@ -1,6 +1,6 @@
 
 exports.up = async function(knex) {
-    await knex.schema.createTable('user', tbl=>{
+    await knex.schema.createTable('users', tbl=>{
         tbl.increments();// creates id and increments automatically
         tbl.string('username', 128).unique().notNullable();
         tbl.string('password', 128).notNullable();
